262aaa95f1f0e284b6199c9eb40429e7f5c057a6
Anetor Oselumese <anetoroselumese@gmail.com>
2023-08-29 02:30:56 +0100

    config

--- Program.cs
+++ Program.cs
@@ -27,6 +27,7 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
+    app.UseStatusCodePagesWithRedirects("/Home/HttpStatusCodeHandler");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
